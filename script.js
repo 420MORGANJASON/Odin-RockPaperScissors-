687a6aba9a7b84745f95048a4061aafc7402b3bc
Morgan Jason <420morganjason@gmail.com>
2024-06-01 19:05:13 +0300

    Create logic to get human choice

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-console.log("Hello world")
+// console.log("Hello world")
 
 function getComputerChoice() {
     let randomNum = Math.random()
 
     if (randomNum < 1 / 3) {
-        return "Rock";
+        return "Computer Choice:Rock";
     } else if ( randomNum < 2 / 3) {
-        return "Paper";
+        return "Computer Choice:Paper";
     } else {
-        return "Scissors"
+        return "Computer Choice:Scissors"
     }
+
     }
+    function getHumanChoice() {
+        let humanChoice = prompt(`Input Rock, Paper or Scissors......`)
+
+        if( humanChoice === "Rock") {
+            return "Human Choice:Rock"
+        } else if ( humanChoice === "Scissors") {
+            return "Human Choice:Scissors"
+        } else if ( humanChoice === "Paper") {
+            return "Human Choice:Paper" 
+        } else {
+            return "Sorry!The game only allows.Rock,Paper or Scissors.Try again!!!"
+        }
+            
+    }
+    console.log(getHumanChoice())
+    
+
 console.log(getComputerChoice())
+
