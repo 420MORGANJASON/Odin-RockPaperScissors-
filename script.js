7b1a962cfa39a2f76ebe4549c7d4bce90ed3e5c5
Morgan Jason <420morganjason@gmail.com>
2025-02-07 22:31:32 +0300

    get computer choice logic

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-// console.log("Hello world")
-let humanScore = 0;
-let computerScore = 0;
+// CREATE getComputerChoice function
+// getComputerChoice randomly RETURN rock,paper,scissors
+// IF compChoice
 
 function getComputerChoice() {
-    let randomNum = Math.random();
+  const compChoice = Math.random();
+  if (compChoice < 0.2) {
+    console.log("Rock");
+  } else if (compChoice > 0.2 && compChoice < 0.6) {
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+  return compChoice;
+}
 
-    if (randomNum < 1 / 3) {
-        return "Computer Choice:Rock";
-    } else if ( randomNum < 2 / 3) {
-        return "Computer Choice:Paper";
-    } else {
-        return "Computer Choice:Scissors"
-    }
 
-    }
-    function getHumanChoice() {
-        let humanChoice = prompt(`Input Rock, Paper or Scissors......`)
-
-        if( humanChoice === "Rock") {
-            return "Human Choice:Rock"
-        } else if ( humanChoice === "Scissors") {
-            return "Human Choice:Scissors"
-        } else if ( humanChoice === "Paper") {
-            return "Human Choice:Paper" 
-        } else {
-            return "Sorry!The game only allows.Rock,Paper or Scissors.Try again!!!"
-        }
-            
-    }
-    console.log(getHumanChoice())
-    
-
-console.log(getComputerChoice())
-
+console.log(getComputerChoice());
